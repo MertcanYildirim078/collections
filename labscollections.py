466bfd761c550d59d70ec699425cb06847923819
MertcanYildirim078 <99068314@mydavinci.nl>
2021-12-08 10:07:40 +0100

    'labs'

---
+++ labscollections.py
@@ -0,0 +1,55 @@
+week = ['maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag', 'zondag']
+print(week)
+for a in range(0,5):
+    print(week[a])
+print('')
+for b in range(5,7):
+    print(week[b])
+print('')
+for c in range(6,-1,-1):
+    print(week[c])
+print('')
+for d in range(4,-1,-1):
+    print(week[d])
+print('')
+for e in range(6,4,-1):
+    print(week[e])
+
+
+
+
+
+
+
+listone = [1,2,3,4,5,6,7,8,9,10]
+listtwo = [2,4,6,8,10,12,14,16,18,20]
+
+def addAndDisplayLists():
+    listone = [1,2,3,4,5,6,7,8,9,10]
+    listtwo = [2,4,6,8,10,12,14,16,18,20]
+    print('1 + 2 = ', listone.index(1) + listtwo.index(2))
+    print('2 + 4 = ', listone.index(2) + listtwo.index(4))
+    print('3 + 6 = ', listone.index(3) + listtwo.index(6))
+    print('4 + 8 = ', listone.index(4) + listtwo.index(8))
+    print('5 + 10 = ', listone.index(5) + listtwo.index(10))
+    print('6 + 12 = ', listone.index(6) + listtwo.index(12))
+    print('7 + 14 = ', listone.index(7) + listtwo.index(14))
+    print('8 + 16 = ', listone.index(8) + listtwo.index(16))
+    print('9 + 18 = ', listone.index(9) + listtwo.index(18))
+    print('10 + 20 = ', listone.index(10) + listtwo.index(20))
+
+    
+
+
+addAndDisplayLists()
+
+
+SpelList = {'Monopoly', 'Yathzee', 'Bridge', 'Poker', 'Pesten', 'Mens erger je niet', 'Cluedo'}
+def spelProgramma(SpelList):
+    SpelList = {'Monopoly', 'Yathzee', 'Bridge', 'Poker', 'Pesten', 'Mens erger je niet', 'Cluedo'}
+    print(min, 3, max, 10, SpelList)
+spelProgramma(SpelList)
+
+
+
+
